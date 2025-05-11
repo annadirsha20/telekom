33c842d5496e79a2534bda76678b1aae759b4585
ArtemisGur <love-gura@yandex.ru>
2025-05-11 21:04:07 +0700

    add new pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { About } from './about/about.component';
+import { AuthPage } from './auth/auth.component';
+
+const routes: Routes = [
+  { path: 'about', component: About },
+  { path: 'auth', component: AuthPage },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { NavMenu } from './nav-menu/nav-menu.component';
+import { About } from './about/about.component';
 import { AppComponent } from './app.component';
+import { AuthPage } from './auth/auth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenu,
+    About,
+    AuthPage
   ],
   imports: [
     BrowserModule,
